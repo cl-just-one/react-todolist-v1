b7c9744e93fb27addf2846b8d452f11c6e1b2f57
cl-just-one <837460387@qq.com>
2019-07-02 23:15:58 +0800

    Redux

---
+++ src/store/reducer.js
@@ -0,0 +1,28 @@
+import { CHANGE_INPUT_VALUE, ADD_TODO_ITEM, DELETE_TODO_ITEM } from './actionTypes.js';
+
+const defaultStore = {
+  inputValue: '',
+  list: []
+}
+
+// reducer可以接收state， 但是绝不能修改state
+// 纯函数 给定固定的输入 具有固定的输出 一定没有副作用
+export default (store = defaultStore, action) => {
+  if (action.type === CHANGE_INPUT_VALUE) {
+    const newState = JSON.parse(JSON.stringify(store));
+    newState.inputValue = action.value;
+    return newState;
+  }
+  if (action.type === ADD_TODO_ITEM) {
+    const newState = JSON.parse(JSON.stringify(store));
+    newState.list.push(newState.inputValue);
+    newState.inputValue = '';
+    return newState;
+  }
+  if (action.type === DELETE_TODO_ITEM) {
+    const newState = JSON.parse(JSON.stringify(store));
+    newState.list.splice(action.index, 1);
+    return newState;
+  }
+  return store;
+}
